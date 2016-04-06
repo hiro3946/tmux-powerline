3dad6d58b066426e41838753e2104f45105c37e9
hiro <hiro@example.com>
2016-04-07 00:41:43 +0900

    del weather

--- themes/mytheme.sh
+++ themes/mytheme.sh
@@ -24,7 +24,7 @@ TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SE
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
 		#"tmux_session_info 148 234" \
-	        "tmux_session_info 11 0" \
+	        "tmux_session_info 100 0" \
 		#"hostname 235 136" \
 		#"ifstat 30 255" \
 		"ifstat_sys 2 0" \
@@ -45,16 +45,19 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 		#"mailcount 9 255" \
 		#"now_playing 234 37" \
 		#"cpu 240 136" \
-		"load 137 255" \
+		"load 137 0" \
 		#"tmux_mem_cpu_load 234 136" \
-		#"battery 235 136" \
+		"battery 142 0" \
 		#"weather 37 255" \
-		#"weather 137 255" \
+		#"weather 137 0" \
 		#"rainbarf 0 0" \
 		#"xkb_layout 125 117" \
-		"date_day 235 136" \
-		"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		#"date_day 235 136" \
+		#"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		#"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		"date_day 130 255" \
+		"date 130 255" \
+		"time 130 255" \
 		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 	)
 fi
